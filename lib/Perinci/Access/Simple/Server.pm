0494d3ce82f866741eaa36da714e1a990b657a52
Steven Haryanto <stevenharyanto@gmail.com>
2012-08-15 02:10:41 +0000

    Import of SHARYANTO/Perinci-Access-Simple-Server-0.11 from CPAN.
    gitpan-cpan-distribution: Perinci-Access-Simple-Server
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         SHARYANTO/Perinci-Access-Simple-Server-0.11.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Perinci/Access/Simple/Server.pm
+++ lib/Perinci/Access/Simple/Server.pm
@@ -1,6 +1,6 @@
 package Perinci::Access::Simple::Server;
 
-our $VERSION = '0.10'; # VERSION
+our $VERSION = '0.11'; # VERSION
 
 1;
 # ABSTRACT: Riap::Simple server implementations
@@ -15,7 +15,7 @@ Perinci::Access::Simple::Server - Riap::Simple server implementations
 
 =head1 VERSION
 
-version 0.10
+version 0.11
 
 =head1 SEE ALSO
 
